e691def7442b9d8cf63aa1e5de9911ce1aad2f64
Sivakov Oleg <meng263@gmail.com>
2022-10-05 14:06:30 +0300

    added ResourceQuestionProviderTest

---
+++ spring_introduction/src/test/java/ru/otus/service/ResourceQuestionProviderTest.java
@@ -0,0 +1,20 @@
+package ru.otus.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.otus.model.Answer;
+import ru.otus.model.Question;
+
+import java.util.List;
+
+class ResourceQuestionProviderTest {
+    @Test
+    public void correctParseQuestionsFromResourceFile() {
+        String csvPath = "test_questions.csv";
+        ResourceQuestionProvider qp = new ResourceQuestionProvider(csvPath);
+        List<Question> result = qp.getAllQuestions();
+        List<Question> expected = List.of(new Question("How many oceans are there in the world?",
+                List.of(new Answer("3"), new Answer("4"), new Answer("5"))));
+        Assertions.assertEquals(result, expected);
+    }
+}
