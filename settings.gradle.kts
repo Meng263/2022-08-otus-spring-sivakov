caa0fb50413e4d350e6cd65f23b9180e78310572
Sivakov Oleg <meng263@gmail.com>
2022-10-12 09:02:29 +0300

    add new module for homework

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,5 @@
 rootProject.name = "otus-spring-2022-08-sivakov"
-include("spring_introduction")
+include(
+    "spring_introduction_01",
+    "test_students_02"
+)
---
+++ test_students_02/src/main/java/ru/otus/Application.java
@@ -0,0 +1,6 @@
+package ru.otus;
+
+public class Application {
+    public static void main(String[] args) {
+    }
+}
